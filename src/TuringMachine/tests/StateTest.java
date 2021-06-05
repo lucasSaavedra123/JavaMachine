2bb79bb2bc6685a4253a209ba67eb09ba552a45b
lucassaavedra123 <lucasarielsaavedra@hotmail.com>
2021-06-05 13:24:44 -0300

    Added dependencies

--- src/TuringMachine/tests/StateTest.java
+++ src/TuringMachine/tests/StateTest.java
@@ -1,4 +1,4 @@
-package TuringMachine.tests;
+package TuringMachine.Tests;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
